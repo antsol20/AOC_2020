61ba3962ac7b7b66c33f3edc7c45d49246fa0e05
antsol20 <antsol20@hotmail.com>
2021-12-01 20:04:13 +0000

    x

--- day6.py
+++ day6.py
@@ -4,3 +4,24 @@ f = open('input.txt', 'r')
 
 for line in f:
     input.append(line.strip())
+
+total_answers = []
+answers = []
+
+for line in input:
+
+    if line == "":
+        total_answers.append(set(answers))
+        answers = []
+
+    else:
+        for chr in line:
+            answers.append(chr)
+
+print(len(total_answers[0]))
+print(len(total_answers[1]))
+counter = 0
+for res in total_answers:
+    counter += len(res)
+
+print(counter)
