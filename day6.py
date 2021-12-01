3c6d61ca7d7cd498efb03e9ca4d4812d9cc94e6d
antsol20 <antsol20@hotmail.com>
2021-12-01 20:52:24 +0000

    x

--- day6.py
+++ day6.py
@@ -1,3 +1,4 @@
+import string
 input = []
 
 f = open('input.txt', 'r')
@@ -18,10 +19,25 @@ for line in input:
         for chr in line:
             answers.append(chr)
 
-print(len(total_answers[0]))
-print(len(total_answers[1]))
 counter = 0
 for res in total_answers:
     counter += len(res)
 
+print("Part 1:")
+print(counter)
+
+# ########################part 2#############
+all_yes = set(string.ascii_lowercase)
+counter = 0
+for line in input:
+    if line == "":
+        counter += len(all_yes)
+        all_yes = set(string.ascii_lowercase)
+
+    else:
+        for letter in set(string.ascii_lowercase):
+            if letter not in line and letter in all_yes:
+                all_yes.remove(letter)
+
+print("Part 2:")
 print(counter)
